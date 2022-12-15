553288a9853228f72d0baf3d8a60670a06c47e2d
papademos <emil.ingberg@gmail.com>
2022-12-15 20:35:28 +0100

    Created a simple tunnel effect using SharpDX/Direct2D/System.Numerics

---
+++ MathF.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Jolt.Math;
+
+// Misc math helper methods, copied from one of my private repos.
+// Note: Angles are expressed in Revolutions. (1 Revolution = 360 degrees = 2*PI).
+static class MathF
+{
+    //
+    public const float PI = (float)System.Math.PI;
+    public const float Epsilon = 1e-6f;
+
+    //
+    public static bool IsZero(float x) => Abs(x) < Epsilon;
+    public static int Sign(float x) => IsZero(x) ? 0 : System.Math.Sign(x);
+
+    //
+    public static int Mod(int x, int y) => (x % y + y) % y;
+    public static float Abs(float x) => x < 0 ? -x : x;
+    public static float Pow(float x, float y) => (float)System.Math.Pow(x, y);
+    public static float Sqrt(float x) => (float)System.Math.Sqrt(x);
+
+    //
+    public static int Min(int x, int y) => x < y ? x : y;
+    public static int Max(int x, int y) => x > y ? x : y;
+    public static long Min(long x, long y) => x < y ? x : y;
+    public static long Max(long x, long y) => x > y ? x : y;
+    public static float Min(float x, float y) => x < y ? x : y;
+    public static float Max(float x, float y) => x > y ? x : y;
+    public static float Clamp(float x, float min, float max) => x < min ? min : x < max ? x : max;
+    public static float Saturate(float x) => Clamp(x, 0, 1);
+
+    //
+    public static float RadToRev(float rad) => rad / 2 / (float)System.Math.PI;
+    public static float RevToRad(float rev) => rev * 2 * (float)System.Math.PI;
+
+    //
+    public static float Sin(float a) => (float)System.Math.Sin(RevToRad(a));
+    public static float Cos(float a) => (float)System.Math.Cos(RevToRad(a));
+    public static float Asin(float value) => RadToRev((float)System.Math.Asin(value));
+    public static float Acos(float value) => RadToRev((float)System.Math.Acos(value));
+    public static float Atan(float value) => RadToRev((float)System.Math.Atan(value));
+    public static float Atan(float y, float x) => RadToRev((float)System.Math.Atan2(y, x));
+
+    //
+    public static float Lerp(float min, float max, float t) => min + t * (max - min);
+    public static float Angle(Vector3 u, Vector3 v) => Acos(Vector3.Dot(u, v));
+    public static float Cross(Vector2 u, Vector2 v) => u.X * v.Y - u.Y * v.X; // Note: This is basically the Z result of the 3D cross product.
+
+    //
+    public static Matrix4x4 RotX(float a) => Matrix4x4.CreateRotationX(RevToRad(a));
+    public static Matrix4x4 RotY(float a) => Matrix4x4.CreateRotationY(RevToRad(a));
+    public static Matrix4x4 RotZ(float a) => Matrix4x4.CreateRotationZ(RevToRad(a));
+    public static Matrix4x4 Translate(float x, float y, float z) => Matrix4x4.CreateTranslation(x, y, z);
+    public static Matrix4x4 Perspective(float w, float h, float near, float far) => Matrix4x4.CreatePerspective(w, h, near, far);
+
+    //
+    public static Vector2 V(float x, float y) => new(x, y);
+    public static Vector3 V(float x, float y, float z) => new(x, y, z);
+    public static Vector4 V(float x, float y, float z, float w) => new(x, y, z, w);
+    public static Vector2 V2(Vector3 v3) => new(v3.X, v3.Y);
+    public static Vector2 V2(Vector4 v4) => new(v4.X, v4.Y);
+    public static Vector3 V3(Vector2 v2) => new(v2.X, v2.Y, 0);
+    public static Vector3 V3(Vector4 v4) => new(v4.X, v4.Y, v4.Z);
+    public static Vector4 V4(Vector3 v3, float w) => new(v3, w);
+    public static Vector4 V4(Vector2 v2) => new(v2.X, v2.Y, 0, 0);
+
+    //
+    public static IEnumerable<Vector4> Transform(this IEnumerable<Vector4> positions, Matrix4x4 m)
+        => positions.Select(p => Vector4.Transform(p, m));
+}
---
+++ Program.cs
@@ -0,0 +1,175 @@
+﻿using Jolt.Math;
+using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using static Jolt.Math.MathF;
+using AlphaMode = SharpDX.Direct2D1.AlphaMode;
+using D2DFactory = SharpDX.Direct2D1.Factory;
+using Device = SharpDX.Direct3D11.Device;
+using DxgiFactory1 = SharpDX.DXGI.Factory1;
+using Vector4 = System.Numerics.Vector4;
+
+// Create a Form for rendering.
+var form = new RenderForm {
+    Text = "Joltprickar till Dylte",
+    Icon = null,
+    WindowState = FormWindowState.Maximized
+};
+
+// Listen to keyboard events and close the form on escape.
+form.KeyDown += (source, args) => {
+    switch (args.KeyCode) {
+        case Keys.Escape:
+            form.Dispose();
+            break;
+    }
+};
+
+// Create factories needed throughout the application.
+using var dxgiFactory = new DxgiFactory1();
+using var d2dFactory = new D2DFactory();
+
+// Create the Device, which is our main interface to DirectX.
+using var device = new Device(
+    DriverType.Hardware,
+    DeviceCreationFlags.BgraSupport,
+    new SharpDX.Direct3D.FeatureLevel[] { SharpDX.Direct3D.FeatureLevel.Level_11_1 });
+
+// Create a SwapChain so we have something to render to.
+using var swapChain = new SwapChain(dxgiFactory, device, new SwapChainDescription {
+    BufferCount = 1,
+    ModeDescription = new ModeDescription(
+        form.ClientSize.Width,
+        form.ClientSize.Height,
+        new Rational(60, 1),
+        Format.R8G8B8A8_UNorm),
+    IsWindowed = true,
+    OutputHandle = form.Handle,
+    SampleDescription = new SampleDescription(1, 0),
+    SwapEffect = SwapEffect.Discard,
+    Usage = Usage.RenderTargetOutput
+});
+
+// Resources needed for rendering to the SwapChain buffers.
+var backBuffer = null as Texture2D;
+var surface = null! as Surface;
+var renderTarget = null! as RenderTarget;
+
+// Calculate the positions for a thorus.
+var thorus = CreateThorus(96, 1000, 32, 100);
+
+// Create a stopwatch so we can have something to track time.
+var stopwatch = Stopwatch.StartNew();
+
+// Main loop. (RenderFrame is incoked once per frame.)
+RenderLoop.Run(form, RenderFrame);
+
+// Clean up resources and quit the application.
+renderTarget?.Dispose();
+surface?.Dispose();
+backBuffer?.Dispose();
+device.ImmediateContext.ClearState();
+device.ImmediateContext.Flush();
+return;
+
+void RenderFrame() {
+    var t = (float)stopwatch.Elapsed.TotalSeconds;
+    var (w, h) = (form.ClientSize.Width, form.ClientSize.Height);
+
+    UpdateResources(w, h, d2dFactory, swapChain,  ref backBuffer, ref surface, ref renderTarget);
+    UpdateGeometry(t, w, h, thorus, out var positions);
+    Render(d2dFactory, renderTarget!, positions);
+    
+    // Copy the rendered result to the screen.
+    swapChain.Present(0, PresentFlags.None);
+};
+
+static void UpdateGeometry(float t, float w, float h, Vector4[] thorus, out Vector4[] positions) {
+    var aspect = 1f * w / h;
+    var znear = 10f;
+    var zfar = 1001f;
+
+    Vector4 TransformToScreen(Vector4 p) {
+        p.X /= p.W;
+        p.Y /= p.W;
+        p.X = p.X * w + w / 2;
+        p.Y = p.Y * h + h / 2;
+        return p;
+    };
+    
+    positions = thorus
+        .Transform(
+            RotX(0.1f * t) *
+            Translate(0, 900, 500) *
+            RotZ(0.15f * t))
+        .Where(p => p.Z > 100)
+        .Transform(Perspective(10 * aspect, 10, znear, zfar))
+        .Select(TransformToScreen)
+        .ToArray();
+}
+
+static void UpdateResources(int w, int h, 
+    D2DFactory factory,
+    SwapChain swapChain, 
+    ref Texture2D? backBuffer, 
+    ref Surface? surface, 
+    ref RenderTarget? renderTarget) {
+
+    var isDirty = 
+        w != swapChain.Description.ModeDescription.Width ||
+        h != swapChain.Description.ModeDescription.Height;
+    if (isDirty) {
+        // Dispose old resources.
+        Utilities.Dispose(ref renderTarget);
+        Utilities.Dispose(ref surface);
+        Utilities.Dispose(ref backBuffer);
+
+        // Resize or Create SwapChain.
+        swapChain.ResizeBuffers(swapChain.Description.BufferCount, w, h, Format.Unknown, SwapChainFlags.None);
+    }
+
+    // (Re)Create all resources needed to get a RenderTarget.
+    backBuffer ??= Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+    surface ??= backBuffer.QueryInterface<Surface>();
+    renderTarget ??= new RenderTarget(factory, surface, new(new(Format.Unknown, AlphaMode.Premultiplied)));
+}
+
+static void Render(D2DFactory factory, RenderTarget renderTarget, Vector4[] positions) {
+    renderTarget.BeginDraw();
+    renderTarget.Clear(Color.Black);
+    foreach (var p in positions) {
+        var (x, y) = (p.X, p.Y);
+        var s = Saturate(1 - p.Z / -1400);
+        var radius = s * 10;
+        var color = new Color4(0, 1, 1, s);
+        FillEllipse(factory, renderTarget, color, x, y, radius, radius);
+    }
+    renderTarget.EndDraw();
+}
+
+static Vector4[] CreateCircle(int count, float radius)
+    => Enumerable
+    .Range(0, count)
+    .Select(i => (float)i / count)
+    .Select(a => new Vector4(radius * Cos(a), radius * Sin(a), 0, 1))
+    .ToArray();
+
+static Vector4[] CreateThorus(int largeCount, float largeRadius, int smallCount, float smallRadius)
+    => Enumerable
+    .Repeat(CreateCircle(smallCount, smallRadius), largeCount)
+    .SelectMany((circle, i) => circle.Transform(
+        Translate(0, largeRadius, 0) *
+        RotX((float)i / largeCount)))
+    .ToArray();
+
+static void FillEllipse(D2DFactory factory, RenderTarget target, Color4 color, float x, float y, float radiusX, float radiusY) {
+    using var brush = new SolidColorBrush(target, color);
+    using var geometry = new EllipseGeometry(factory, new(new(x, y), radiusX, radiusY));
+    target.FillGeometry(geometry, brush, null);
+}
